b06cb11dd56a556391c8e843de552f5fc41f9360
emese789 <56880067+emese789@users.noreply.github.com>
2021-03-16 14:31:21 +0200

    Create and Implement SplashScreenActivity

---
+++ app/src/main/java/com/example/movieapp/Activities/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.movieapp.Activities;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.movieapp.LoginActivity;
+import com.example.movieapp.R;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(LoginActivity.class)
+                .withSplashTimeOut(3000)
+                .withBackgroundColor(Color.parseColor("#E6000000"))
+                .withLogo(R.drawable.logo);
+
+        getSupportActionBar().hide();
+        View easySplashScreen = config.create();
+        setContentView(easySplashScreen);
+    }
+}
