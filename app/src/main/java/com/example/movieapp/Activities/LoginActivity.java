4d9333a6e88f3bf0de63089758ce4e91ae5ec083
emese789 <56880067+emese789@users.noreply.github.com>
2021-04-14 16:19:39 +0300

    Add Remember me checkbox to Login Screen

--- app/src/main/java/com/example/movieapp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/movieapp/Activities/LoginActivity.java
@@ -4,10 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText emailET, passwordET;
     Button loginBtn, registerBtn;
     FirebaseAuth fAuth;
+    CheckBox rememberMeCHB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +38,21 @@ public class LoginActivity extends AppCompatActivity {
         passwordET = findViewById(R.id.PasswordText);
         loginBtn = findViewById(R.id.LoginButton);
         registerBtn = findViewById(R.id.SignUpButton);
+        rememberMeCHB = findViewById(R.id.rememberMeCheckBox);
         fAuth = FirebaseAuth.getInstance();
+
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences.Editor editor = preferences.edit();
+        if(preferences.contains("checked") && preferences.getBoolean("checked", false)) {
+            rememberMeCHB.setChecked(true);
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+
+        }else {
+            rememberMeCHB.setChecked(false);
+
+        }
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +85,19 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),RegistrationActivity.class));
+            }
+        });
+
+        rememberMeCHB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b){
+                if(compoundButton.isChecked()){
+                    editor.putBoolean("checked",true);
+                    editor.apply();
+                } else{
+                    editor.putBoolean("checked",false);
+                    editor.apply();
+                }
             }
         });
     }
