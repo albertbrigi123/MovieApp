9d986652e54e4069851459de2a0145cac52aaf6f
albertbrigi123 <55790383+albertbrigi123@users.noreply.github.com>
2021-04-16 14:10:44 +0300

    Create and implement Profile page

--- app/src/main/java/com/example/movieapp/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/movieapp/Activities/ProfileActivity.java
@@ -4,18 +4,44 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.movieapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ProfileActivity extends AppCompatActivity {
     TextView emailTW, nameTW;
     ImageView profileImage;
+    StorageReference storageReference;
+    FirebaseStorage storage;
+    FirebaseFirestore fStore;
+    String userId;
+
+    private static final int RESULT_LOAD_IMAGE=1;
+    private DocumentReference documentReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +50,50 @@ public class ProfileActivity extends AppCompatActivity {
         emailTW = findViewById(R.id.Email);
         nameTW = findViewById(R.id.Name);
         profileImage = findViewById(R.id.profile_image);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        fStore = FirebaseFirestore.getInstance();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        userId = FirebaseAuth.getInstance().getUid();
+        documentReference = fStore.collection("users").document(userId);
+        documentReference.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists())
+                        {
+                            String fName = documentSnapshot.getString("firstName");
+                            String lName = documentSnapshot.getString("lastName");
+                            String email = documentSnapshot.getString("email");
+
+                            nameTW.setText(fName + " " + lName);
+                            emailTW.setText(email);
+                        }
+                        else
+                        {
+                            Toast.makeText(ProfileActivity.this, "Document does not exists", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery,RESULT_LOAD_IMAGE);
+            }
+        });
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
@@ -45,5 +115,16 @@ public class ProfileActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data!=null)
+        {
+            Uri selectedImage = data.getData();
+            String path = selectedImage.toString();
+            profileImage.setImageURI(selectedImage);
+        }
     }
 }
